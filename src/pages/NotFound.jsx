1a836aa75762a7ec1d2f9f6157428a0d6ab77e69
Fatma Elraey <FatmaMahmoudElraey>
2025-02-23 19:29:37 +0200

    customer view updates

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function NotFound() {
-    return (
-        <div className='notfound d-flex justify-content-center align-items-center'>
-            <h1 className='text-muted'><img src='../src/img/error.png' class="w-100"/></h1>
-        </div>
-    )
-}
